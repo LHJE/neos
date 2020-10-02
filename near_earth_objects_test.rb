2bbc61b37e56c583c967cb40dc184604a86174a3
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-02 13:06:00 -0600

    Create new tests to make helper methods

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -8,4 +8,42 @@ class NearEarthObjectsTest < Minitest::Test
     results = NearEarthObjects.find_neos_by_date('2019-03-30')
     assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
   end
+
+  def test_get_list_data
+    date = '2019-03-30'
+    require "pry"; binding.pry
+
+    asteroids_list_data = get_list_data(date)
+
+    assert_equal Faraday::Response, asteroids_list_data.class
+  end
+
+  def test_parse_astreroids_data
+    date = '2019-03-30'
+    asteroids_list_data = get_list_data(date)
+
+    parsed_asteroids_data = parse_asteroids_data(asteroids_list_data, date)
+
+    assert_equal "3840858", parsed_asteroids_data[0][:id]
+  end
+
+  def test_get_formatted_asteroid_data
+    date = '2019-03-30'
+    asteroids_list_data = get_list_data(date)
+    parsed_asteroids_data = parse_asteroids_data(asteroids_list_data, date)
+
+    formatted_asteroid_data = get_formatted_asteroid_data(parsed_asteroids_data)
+
+    assert_equal "(2019 GD4)", formatted_asteroid_data[0][:name]
+  end
+
+  def test_get_largest_asteroid_diameter
+    date = '2019-03-30'
+    asteroids_list_data = get_list_data(date)
+    parsed_asteroids_data = parse_asteroids_data(asteroids_list_data, date)
+
+    largest_asteroid_diameter = get_largest_asteroid_diameter(parsed_asteroids_data)
+
+    assert_equal 10233, largest_asteroid_diameter
+  end
 end
